b97cd88ed77e90f089497ef4de9914acd1863c6a
unknown <sastraharapangulo2002@gmail.com>
2022-11-18 22:13:35 +0700

    update

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 import Grow from "@mui/material/Grow";
@@ -47,30 +47,92 @@ const Nav = () => {
 
   return (
     <div>
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light">
-          <div className="container">
-            <img src="./images/logo.png" alt="" />
-            {/* <!-- <a className="navbar-brand" href="#"></a> --> */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasWithBothOptions"
-              aria-controls="offcanvasWithBothOptions"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav justify-content-end">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+        <div className="container">
+          <img src="./images/logo.png" alt="" />
+          {/* <!-- <a className="navbar-brand" href="#"></a> --> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasWithBothOptions"
+            aria-controls="offcanvasWithBothOptions"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav justify-content-end">
+              <li className="nav-item me-2">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link" href="#Why-Us">
+                  Our Services
+                </a>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link" href="#Why-Us">
+                  Why Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#testimonial">
+                  Testimonial
+                </a>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link" href="#FAQ">
+                  FAQ
+                </a>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="btn btn-primary btn-sm"
+                  ref={anchorRef}
+                  id="composition-button"
+                  aria-controls={open ? "composition-menu" : undefined}
+                  aria-expanded={open ? "true" : undefined}
+                  aria-haspopup="true"
+                  onClick={handleToggle}
+                >
+                  {isLoggedIn ? "Registered" : "Login"}
+                </button>
+              </li>
+            </ul>
+          </div>
+
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
+            id="offcanvasWithBothOptions"
+            aria-labelledby="offcanvasWithBothOptionsLabel"
+          >
+            <div className="offcanvas-header">
+              <h3
+                className="offcanvas-title"
+                id="offcanvasWithBothOptionsLabel"
+              >
+                BCR
+              </h3>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav">
                 <li className="nav-item me-2">
-                  <Link to="/" className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item me-2">
-                  <a className="nav-link" href="#Why-Us">
+                  <a
+                    className="nav-link active"
+                    aria-current="page"
+                    href="#Our-Services"
+                  >
                     Our Services
                   </a>
                 </li>
@@ -91,121 +153,57 @@ const Nav = () => {
                 </li>
                 <li className="nav-item">
                   <button
-                    className="btn btn-primary btn-sm"
-                    ref={anchorRef}
-                    id="composition-button"
-                    aria-controls={open ? "composition-menu" : undefined}
-                    aria-expanded={open ? "true" : undefined}
+                    type="button"
                     aria-haspopup="true"
                     onClick={handleToggle}
                   >
-                    {isLoggedIn ? ("Registered"): ("Login")}
+                    {isLoggedIn ? "Registered" : "Login"}
                   </button>
                 </li>
               </ul>
             </div>
-
-            <div
-              className="offcanvas offcanvas-end"
-              tabIndex="-1"
-              id="offcanvasWithBothOptions"
-              aria-labelledby="offcanvasWithBothOptionsLabel"
-            >
-              <div className="offcanvas-header">
-                <h3
-                  className="offcanvas-title"
-                  id="offcanvasWithBothOptionsLabel"
-                >
-                  BCR
-                </h3>
-                <button
-                  type="button"
-                  className="btn-close text-reset"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="offcanvas-body">
-                <ul className="navbar-nav">
-                  <li className="nav-item me-2">
-                    <a
-                      className="nav-link active"
-                      aria-current="page"
-                      href="#Our-Services"
-                    >
-                      Our Services
-                    </a>
-                  </li>
-                  <li className="nav-item me-2">
-                    <a className="nav-link" href="#Why-Us">
-                      Why Us
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#testimonial">
-                      Testimonial
-                    </a>
-                  </li>
-                  <li className="nav-item me-2">
-                    <a className="nav-link" href="#FAQ">
-                      FAQ
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    {/* <Button 
-                                        type="button" 
-                                        className="btn btn-primary btn-sm"
-                                        ref={anchorRef}
-                                        id="composition-button"
-                                        aria-controls={open ? 'composition-menu' : undefined}
-                                        aria-expanded={open ? 'true' : undefined}
-                                        aria-haspopup="true"
-                                        onClick={handleToggle}
-                                    >
-                                        Register
-                                    </Button> */}
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
-          <Popper
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            placement="bottom-start"
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === "bottom-start" ? "left top" : "left bottom",
-                }}
-              >
-                <Paper>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList
-                      autoFocusItem={open}
-                      id="composition-menu"
-                      aria-labelledby="composition-button"
-                      onKeyDown={handleListKeyDown}
-                    >
-                      <MenuItem onClick={handleClose}>
-                        {isLoggedIn ? (
-                          <a onClick={handleLogout} href>Logout</a>
-                        ):(<Link to="/Login">Login</Link>)}
-                        
-                      </MenuItem>
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-        </nav>
+        </div>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      {isLoggedIn ? (
+                        <a onClick={handleLogout} href>
+                          Logout
+                        </a>
+                      ) : (
+                        <Link to="/Login">Login</Link>
+                      )}
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </nav>
     </div>
   );
 };
